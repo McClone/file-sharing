61412aeda11ecf48889d818977a1a2a8cb70a873
mcclone <abc15606@163.com>
2017-01-21 14:30:20 +0800

    重构

--- file-sharing/src/main/java/org/mcclone/config/SimpleAuditorAwareBean.java
+++ file-sharing/src/main/java/org/mcclone/config/SimpleAuditorAwareBean.java
@@ -3,6 +3,7 @@ package org.mcclone.config;
 import org.mcclone.domain.entity.User;
 import org.mcclone.security.UserPrincipal;
 import org.mcclone.utils.SecurityUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
  * Created by mcclone on 17-1-12.
  */
 @Configuration
+@ConditionalOnProperty(prefix = "spring.data.jpa.repositories", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class SimpleAuditorAwareBean implements AuditorAware<User> {
 
     @Override
--- file-sharing/src/main/java/org/mcclone/web/controller/LoginController.java
+++ file-sharing/src/main/java/org/mcclone/web/controller/LoginController.java
@@ -1,18 +1,40 @@
 package org.mcclone.web.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by mcclone on 17-1-12.
  */
-@Controller
+@RestController
+@Slf4j
+@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping("login")
+    @GetMapping
     public ModelAndView loginPage() {
         return new ModelAndView("/login");
+    }
+
+    @PostMapping
+    public ResponseEntity<String> login(HttpServletRequest request, String username, String password) {
+        try {
+            request.login(username, password);
+        } catch (ServletException e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>("登录成功", HttpStatus.OK);
     }
 
 }
